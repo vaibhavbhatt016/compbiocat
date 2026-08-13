1a195c9e42e32468b20935f37b61b6fb29527875
vaibhavbhatt016 <vaibhavbhatt016@gmail.com>
2026-08-13 19:00:34 +0530

    Fix Vite base path and React Router Basename

--- src/App.jsx
+++ src/App.jsx
@@ -10,7 +10,7 @@ import Contact from './pages/Contact';
 
 function App() {
   return (
-    <Router>
+    <Router basename={import.meta.env.BASE_URL}>
       <PageLayout>
         <Routes>
           <Route path="/" element={<Home />} />
